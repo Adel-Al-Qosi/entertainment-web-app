8935de874ff3d49c67ddd224166fd5161853a92f
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-06-09 20:33:48 +0300

    finish the cards in trending

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -1,67 +1,95 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState, useRef, useLayoutEffect } from "react";
 import "./Trending.css";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
+import movieIcon from "../../assets/icon-category-movie.svg";
+import playIcon from '../../assets/icon-play.svg'
 
 function Trending() {
   const data = useSelector((state) => state.data.entries);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const [images, setImages] = useState([]);
+  const [cards, setCards] = useState([]);
   const [width, setWidth] = useState(0);
 
   const trendingMotion = useRef();
 
-  useEffect(() => {
-    setWidth(trendingMotion.current.scrollWidth - trendingMotion.current.offsetWidth)
-    console.log(trendingMotion.current.scrollWidth - trendingMotion.current.offsetWidth)
-    console.log(trendingMotion.current.offsetWidth)
-    console.log(trendingMotion.current.offsetWidth)
-    
-  }, [])
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
+  useLayoutEffect(() => {
+    const updateWidth = () => {
+      const currentWidth = trendingMotion.current.offsetWidth;
+      const scrollWidth = trendingMotion.current.scrollWidth;
+      setWidth(scrollWidth - currentWidth);
     };
 
-    window.addEventListener("resize", handleResize);
+    updateWidth();
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    const requestID = requestAnimationFrame(updateWidth);
 
+    return () => cancelAnimationFrame(requestID);
   }, []);
 
-  useEffect(() => {
-    const largeImages = [];
-    const smallImages = [];
+  useLayoutEffect(() => {
+    const generateCards = () => {
+      const generatedCards = data
+        .filter((item) => item.thumbnail && item.thumbnail.trending)
+        .map((item) => {
+          const { large } = item.thumbnail.trending;
+          const largeImageUrl = `${process.env.PUBLIC_URL}${large}`;
 
-    data.forEach((item) => {
-      if (item.thumbnail && item.thumbnail.trending) {
-        const { small, large } = item.thumbnail.trending;
-        const smallImageUrl = `${process.env.PUBLIC_URL}${small}`;
-        const largeImageUrl = `${process.env.PUBLIC_URL}${large}`;
+          return {
+            image: largeImageUrl,
+            title: item.title,
+            description: {
+              year: item.year,
+              category: item.category,
+              rating: item.rating,
+            },
+            isMarked: item.isBookmarked,
+          };
+        });
 
-        smallImages.push(smallImageUrl);
-        largeImages.push(largeImageUrl);
-      }
-    });
+      setCards(generatedCards);
+    };
 
-    setImages(windowWidth >= 768 ? largeImages : smallImages);
-  }, [data, windowWidth]);
+    generateCards();
+  }, [data]);
 
   return (
     <div className="trending">
       <h1>Trending</h1>
-      <motion.div className="trending-motion" ref={trendingMotion} whileTap={{cursor: 'grabbing'}}>
+      <motion.div
+        className="trending-motion"
+        ref={trendingMotion}
+        whileTap={{ cursor: "grabbing" }}
+      >
         <motion.div
           drag="x"
           dragConstraints={{ right: 0, left: -width }}
           className="inner-trending-motion"
         >
-          {images.map((image, index) => (
-            <motion.div key={index} className="trending-image">
-              <img src={image} alt="trending" />
+          {cards.map((card, index) => (
+            <motion.div
+              key={index}
+              className="trending-card"
+              style={{ backgroundImage: `url(${card.image})` }}
+            >
+              <h2 className="card-title">{card.title}</h2>
+              <div className="card-description">
+                <p>{card.description.year}</p>
+                <p className="flex">
+                  <img src={movieIcon} alt="item" /> {card.description.category}
+                </p>
+                <p>{card.description.rating}</p>
+              </div>
+              {card.isMarked ? (
+                <div className="card-mark-full"></div>
+              ) : (
+                <div className="card-mark-empty"></div>
+              )}
+              <div className="card-play">
+                <div className="inner-card-play">
+                  <img src={playIcon} />
+                  <p>Play</p>
+                </div>
+              </div>
             </motion.div>
           ))}
         </motion.div>
